065c09255053819d0009369c88ba782512189f37
SMamai <nafulasmamai@gmail.com>
2022-09-15 16:03:41 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _is lower - checks for character in lowercase
  * @c: the character to check
- * Return: 1 if c is lowercase, else 0
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
